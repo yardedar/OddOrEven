7b514a9c18812802e6c5541766389f01e2ae4e2a
Ryan Heinrich <heinrich.ryan@gmail.com>
2021-09-24 12:30:14 -0400

    Add project files.

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            var answer = Console.ReadLine();
+            var value = Convert.ToInt32(answer);
+
+            if ((value % 2 ) is 0)
+            {
+                Console.WriteLine("The number is even");
+            } else 
+            { Console.WriteLine("The number is odd"); }
+        }
+    }
+}
